ac8657f5c37be5f5239377fd07051cee94ef90f5
bhosq <105439038+bhosq@users.noreply.github.com>
2022-05-12 12:56:50 -0400

    Update dir_enum.py

--- dir_enum.py
+++ dir_enum.py
@@ -12,13 +12,19 @@ ext = ext_list.splitlines()
 if len(sys.argv) == 1 or len(sys.argv) > 2:
 	print("Usage: dir_enum.py <URL>")
 else:
-	print(f"Directory Brute-force @ http://{sys.argv[1]}/ with",ext,"extensions")
-	
+	if "http://" in sys.argv[1]: 
+		print(f"Directory Brute-force @ {sys.argv[1]}/ with",", ".join(ext),"extensions")
+		dir_enum = sys.argv[1] + "/{}.{}"
+	elif "https://" in sys.argv[1]:
+		print(f"Directory Brute-force @ {sys.argv[1]}/ with",", ".join(ext),"extensions")
+		dir_enum = sys.argv[1] + "/{}.{}"
+	else:	
+		dir_enum = "http://" + sys.argv[1] + "/{}.{}"
+		print(f"Directory Brute-force @ http://{sys.argv[1]}/ with",", ".join(ext),"extensions")
 	for i in dir:
 		for x in ext:
-			dir_enum = f"http://{sys.argv[1]}/{i}.{x}"
-			r = requests.get(dir_enum)
+			r = requests.get(dir_enum.format(i,x))
 			if r.status_code==404:
 				pass
 			else:
-				print("URL:",dir_enum," -->  Status: ",r.status_code)
+					print("URL:",dir_enum.format(i,x)," -->  Status: ",r.status_code)
